168be1cc52669462c0861ef52175fe625d269e1b
Jaewoo Park <jwualbert@gmail.com>
2016-06-07 07:06:24 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Splurty::Application.config.secret_key_base = '86b25afcaa475fb2b3d2d9a28e88823185b32ad88d43498d248b515c106535a7e33021be6fd128facaf5c136bb5dda3b903475c86f0b5d003df7ff1fd241edcb'
